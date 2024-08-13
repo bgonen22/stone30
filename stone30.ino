f743f920d53d2d25772d5071e40949844024bb0e
bgonen22 <bgonen22@gmail.com>
2024-08-13 22:46:03 +0300

    time_on_top as fraction of duration

--- stone30.ino
+++ stone30.ino
@@ -15,7 +15,7 @@
 #define SPEED_FACTOR 4 // suppose to make the wave length
 #define DELAY_BETWEEN_LEDS 0 // suppose to make the wave slower
 #define TRACE_SIZE 3
-#define TIME_ON_TOP 50 // the time from 0.5 MAX till MAX
+#define TIME_ON_TOP 0.3 // the presentage time from 0.5 MAX till MAX
 TLC_CHANNEL_TYPE channel;
 int channels[] = {1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 17, 18, 19, 20, 24, 25, 26, 27, 28, 29, 30};
 
@@ -34,11 +34,11 @@ void loop()
       int maxValue = analogRead(BRIGHTNESS_PIN) * 2;
       maxValue = map(maxValue, 0, 2046, 0, 4095);
       uint32_t startMillis = millis() + 50;
-      uint32_t endMillis = startMillis + duration;
+      uint32_t endMillis = startMillis + (1-TIME_ON_TOP)*duration;
       tlc_addFade(channel, 0, 0.5*maxValue, startMillis, endMillis );
-      tlc_addFade(channel, 0.5*maxValue, maxValue, endMillis, endMillis +TIME_ON_TOP);
-      tlc_addFade(channel, maxValue, 0.5*maxValue, endMillis + TIME_ON_TOP, endMillis + 2*TIME_ON_TOP);
-      tlc_addFade(channel, 0.5*maxValue, 0, endMillis + 2*TIME_ON_TOP, endMillis + 2*TIME_ON_TOP + duration);
+      tlc_addFade(channel, 0.5*maxValue, maxValue, endMillis, endMillis +  TIME_ON_TOP*duration);
+      tlc_addFade(channel, maxValue, 0.5*maxValue, endMillis + TIME_ON_TOP*duration, endMillis + 2*(TIME_ON_TOP*duration));
+      tlc_addFade(channel, 0.5*maxValue, 0, endMillis + 2*(TIME_ON_TOP*duration), endMillis + (TIME_ON_TOP+1)*duration);
     }
     int arraySize = sizeof(channels) / sizeof(channels[0]);
     if (index++ == arraySize) { 
